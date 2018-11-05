5a2cec4bcd2db8c324915cc1c32bb2e3e19b57be
cpereirarafa <rafael_c_pereira@hotmail.com>
2018-11-05 02:23:27 +0100

    Hardcoded mockup screens added, non-functional, only designed XML

---
+++ app/src/main/java/com/nemeum/project/nemeumproject/CompanyScenarios.java
@@ -0,0 +1,13 @@
+package com.nemeum.project.nemeumproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompanyScenarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_scenarios);
+    }
+}
---
+++ app/src/main/java/com/nemeum/project/nemeumproject/EventFinder.java
@@ -0,0 +1,38 @@
+package com.nemeum.project.nemeumproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventFinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_finder);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
