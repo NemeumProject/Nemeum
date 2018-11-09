a03732333335138b1f9c47924aeff13c03f3469b
cpereirarafa <rafael_c_pereira@hotmail.com>
2018-11-09 15:59:10 +0100

    Enhanced layouts, updated strings/colors files, added all-dpi mipmap

--- app/src/main/java/com/nemeum/project/nemeumproject/MainActivity.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/MainActivity.java
@@ -17,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.homeButton:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.settingsButton:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.accountButton:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
--- app/src/main/java/com/nemeum/project/nemeumproject/NearScenarios.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/NearScenarios.java
@@ -1,7 +1,13 @@
 package com.nemeum.project.nemeumproject;
-
-import android.support.v7.app.AppCompatActivity;
+import
+android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
 
 public class NearScenarios extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class NearScenarios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_near_scenarios);
+
+        Locale[] locale = Locale.getAvailableLocales();
+        ArrayList<String> cities = new ArrayList<String>();
+        String city;
+
+        for(Locale loc : locale) {
+            city = loc.getDisplayCountry();
+            if(city.length() > 0 && !cities.contains(city)) {
+                cities.add(city);
+            }
+        }
+        Collections.sort(cities, String.CASE_INSENSITIVE_ORDER);
+
+        SpinnerAdapter citiesAdapter = new ArrayAdapter(
+                getBaseContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                cities);
+
+        //ListView listView = (ListView) findViewById(R.id.listview);
+        //listView.setAdapter(countriesAdapter);
+
+        //listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //    @Override
+        //    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //        Toast.makeText(getBaseContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+        //    }
+        //});
     }
 }
