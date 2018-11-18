2c97b5d0ac058d7f052cc32c922957f73e08b832
AntonyMartinez <antony11995@hotmail.com>
2018-11-18 11:43:08 -0500

    Creating a post as a trainer activity

---
+++ app/src/main/java/com/nemeum/project/nemeumproject/activity_Trainer_Posting.java
@@ -0,0 +1,35 @@
+package com.nemeum.project.nemeumproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class activity_Trainer_Posting extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__trainer__posting);
+
+        Spinner spinner = findViewById(R.id.sports_Spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (this,R.array.sports_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
