a82cc72f46ea7e70c4bc04b97142902de7b0553a
Maju Sumanto <maju.tampubolon@gmail.com>
2018-11-19 15:37:13 +0100

    Main Screen but still not finished...

--- app/src/main/java/com/nemeum/project/nemeumproject/ActivityMainMock.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/ActivityMainMock.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class ActivityMainMock extends AppCompatActivity {
 
@@ -11,33 +12,110 @@ public class ActivityMainMock extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_mock);
+
+        ImageButton findevent_btn = findViewById(R.id.findeventicon);
+        ImageButton findfacilities_btn = findViewById(R.id.findfacilitiesicon);
+        ImageButton findscenario_btn = findViewById(R.id.findscrenarioicon);
+        ImageButton findtrainer_btn = findViewById(R.id.findtrainericon);
+
+        ImageButton home_btn = findViewById(R.id.homeicon);
+        ImageButton settings_btn =  findViewById(R.id.settingsicon);
+        ImageButton login_btn = findViewById(R.id.loginicon);
+        ImageButton myaccount_btn = findViewById(R.id.myaccounticon);
+
+        findevent_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findfacilities_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findscenario_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findtrainer_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        home_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                homeOnclick();
+            }
+        });
+
+        settings_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settingsOnclick();
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginOnclick();
+            }
+        });
+
+        myaccount_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myaccountOnclick();
+            }
+        });
+
     }
 
-    public void homeOnclick(View v) {
-        Intent intent = new Intent(ActivityMainMock.this, TrainerMock.class);
-        startActivity(intent);
+    public void homeOnclick() {
+        Intent intent1 = new Intent(ActivityMainMock.this, ActivityMainMock.class);
+        startActivity(intent1);
     }
 
-    public void loginOnclick(View v) {
-        Intent intent = new Intent(ActivityMainMock.this, Login.class);
-        startActivity(intent);
+    public void loginOnclick() {
+        Intent intent2 = new Intent(ActivityMainMock.this, Login.class);
+        startActivity(intent2);
     }
 
-    public void facilitieOnclick(View v) {
+    public void settingsOnclick() {
+        //Intent intent3 = new Intent(ActivityMainMock.this,.class);
+        //startActivity(intent3);
+    }
+
+    public void myaccountOnclick() {
+        //Intent intent4 = new Intent(ActivityMainMock.this, .class);
+        //startActivity(intent4);
+    }
+
+    public void findfacilitiesOnclick() {
         Intent intent = new Intent(ActivityMainMock.this, FacilitiesMock.class);
         startActivity(intent);
     }
 
-    public void scenarioOnclick(View v) {
+    public void findscenarioOnclick() {
         Intent intent = new Intent(ActivityMainMock.this, ScenarioMock.class);
         startActivity(intent);
     }
 
-    public void trainerOnclick(View v) {
+    public void findtrainerOnclick() {
         Intent intent = new Intent(ActivityMainMock.this, TrainerMock.class);
         startActivity(intent);
     }
-    public void findEventOnclick(View v) {
+    public void findEventOnclick() {
         Intent intent = new Intent(ActivityMainMock.this, EventFinder.class);
         startActivity(intent);
     }
