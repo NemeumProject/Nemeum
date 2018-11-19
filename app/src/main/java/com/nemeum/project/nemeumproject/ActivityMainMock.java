5f30dc82dbf0586733e0cd293fd98ee7841f5fa4
Maju Sumanto <maju.tampubolon@gmail.com>
2018-11-19 22:36:00 +0100

    Icon Repairment

--- app/src/main/java/com/nemeum/project/nemeumproject/ActivityMainMock.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/ActivityMainMock.java
@@ -1,28 +1,31 @@
 package com.nemeum.project.nemeumproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class ActivityMainMock extends AppCompatActivity {
 
+    Context appContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_mock);
+        appContext = getApplicationContext();
 
         ImageButton findevent_btn = (ImageButton) findViewById(R.id.findeventicon);
         ImageButton findfacilities_btn = (ImageButton) findViewById(R.id.findfacilitiesicon);
         ImageButton findscenario_btn = (ImageButton) findViewById(R.id.findscrenarioicon);
         ImageButton findtrainer_btn = (ImageButton) findViewById(R.id.findtrainericon);
 
-        ImageButton home_btn = (ImageButton) findViewById(R.id.homeicon);
-        ImageButton settings_btn = (ImageButton)  findViewById(R.id.settingsicon);
-        ImageButton login_btn = (ImageButton) findViewById(R.id.loginicon);
-        ImageButton myaccount_btn = (ImageButton) findViewById(R.id.myaccounticon);
 
         findevent_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,54 +55,33 @@ public class ActivityMainMock extends AppCompatActivity {
             }
         });
 
-        home_btn.setOnClickListener(new View.OnClickListener() {
+        BottomNavigationView menu = findViewById(R.id.navigation);
+        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                homeOnclick();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.homeButton:
+                        Intent intentMain = new Intent(appContext, ActivityMainMock.class);
+                        appContext.startActivity(intentMain);
+                        return true;
+                    case R.id.settingsButton:
+                        Intent intentSettings = new Intent(appContext, SettingsActivity.class);
+                        appContext.startActivity(intentSettings);
+                        return true;
+                    case R.id.loginButton:
+                        Intent intentLogin = new Intent(appContext, Login.class);
+                        appContext.startActivity(intentLogin);
+                        return true;
+                    case R.id.accountButton:
+                        Intent intentAccount = new Intent(getApplicationContext(), TrainerDetail.class);
+                        getApplicationContext().startActivity(intentAccount);
+                        return true;
+                    default:
+                        return false;
+                }
             }
         });
 
-        settings_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                settingsOnclick();
-            }
-        });
-
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loginOnclick();
-            }
-        });
-
-        myaccount_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myaccountOnclick();
-            }
-        });
-
-    }
-
-    public void homeOnclick() {
-        Intent intent1 = new Intent(this, ActivityMainMock.class);
-        startActivity(intent1);
-    }
-
-    public void loginOnclick() {
-        Intent intent2 = new Intent(this, Login.class);
-        startActivity(intent2);
-    }
-
-    public void settingsOnclick() {
-        //Intent intent3 = new Intent(ActivityMainMock.this,.class);
-        //startActivity(intent3);
-    }
-
-    public void myaccountOnclick() {
-        //Intent intent4 = new Intent(ActivityMainMock.this, .class);
-        //startActivity(intent4);
     }
 
     public void findFacilitiesOnclick() {
