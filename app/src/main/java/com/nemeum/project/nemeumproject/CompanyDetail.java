b9d16c6e141a82d05e40df25353f41c152e2be5f
AntonyMartinez <antony11995@hotmail.com>
2018-11-19 14:20:10 -0500

    Adding code to call activity_company_editionp

--- app/src/main/java/com/nemeum/project/nemeumproject/CompanyDetail.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/CompanyDetail.java
@@ -1,7 +1,9 @@
 package com.nemeum.project.nemeumproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CompanyDetail extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CompanyDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_detail);
+    }
+
+    public void editCompanyInfo(View view) {
+        Intent intentEditCompanyInfo = new Intent(getApplicationContext(), activity_company_editionp.class);
+        startActivity(intentEditCompanyInfo);
     }
 }
