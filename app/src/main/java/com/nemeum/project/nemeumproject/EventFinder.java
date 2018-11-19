a4c4f9f884cc9cee87b0a2f1dadf345259b81179
cpereirarafa <rafael_c_pereira@hotmail.com>
2018-11-19 22:55:40 +0100

    Updated back buttons

--- app/src/main/java/com/nemeum/project/nemeumproject/EventFinder.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/EventFinder.java
@@ -5,9 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class EventFinder extends AppCompatActivity {
 
@@ -42,5 +41,9 @@ public class EventFinder extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void getBack(View view) {
+        finish();
     }
 }
