37df5567ac7a25d14374dcd54323ab25ede43e91
AntonyMartinez <antony11995@hotmail.com>
2018-11-26 08:39:18 -0500

    Adding validation fields (not empty fields)

--- app/src/main/java/com/nemeum/project/nemeumproject/activity_login_edition.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/activity_login_edition.java
@@ -2,6 +2,11 @@ package com.nemeum.project.nemeumproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+// Author: Antony Martinez
 
 public class activity_login_edition extends AppCompatActivity {
 
@@ -9,5 +14,50 @@ public class activity_login_edition extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_edition);
+    }
+
+    public void save_Edited_Data(View view) {
+        //set up the variables to validate not empty fields and strings to show as error messages
+        EditText etUserName = (EditText) findViewById(R.id.EmailRegisterEdition);
+        String strUserName = etUserName.getText().toString();
+        String username_Error = getString(R.string.username_Empty);
+
+        EditText etPassword= (EditText) findViewById(R.id.password_Edition);
+        String strPassword = etPassword.getText().toString();
+        String password_Error= getString(R.string.password_Empty);
+
+        EditText etNewPassword= (EditText) findViewById(R.id.new_password_Edition);
+        String strNew_password = etNewPassword.getText().toString();
+        String new_password_Error= getString(R.string.new_Password_Empty);
+
+        EditText etRp_newPassword= (EditText) findViewById(R.id.password_Confirmation_edition);
+        String strRp_new_Password = etRp_newPassword.getText().toString();
+        String Rp_new_password_Error= getString(R.string.new_Password_Empty);
+
+        //Validation of empty fields
+
+        if(TextUtils.isEmpty(strUserName)) {
+            etUserName.setError(username_Error);
+            return;
+        }
+        else if (TextUtils.isEmpty(strPassword)){
+            etPassword.setError(password_Error);
+            return;
+
+        }
+        else if (TextUtils.isEmpty(strNew_password)){
+            etNewPassword.setError(new_password_Error);
+            return;
+
+        }
+        else if (TextUtils.isEmpty(strRp_new_Password)){
+            etRp_newPassword.setError(Rp_new_password_Error);
+            return;
+
+        }
+
+
+
+
     }
 }
