8121e040acdfb0ccf7838a24879f920448c913e3
AntonyMartinez <antony11995@hotmail.com>
2018-11-29 18:56:24 -0500

    Adding on Click action to Finish button and needed strings to validate not empty fields.

--- app/src/main/java/com/nemeum/project/nemeumproject/activity_Trainer_Posting.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/activity_Trainer_Posting.java
@@ -2,9 +2,11 @@ package com.nemeum.project.nemeumproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class activity_Trainer_Posting extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -30,6 +32,62 @@ public class activity_Trainer_Posting extends AppCompatActivity implements Adapt
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void posting_Trainer(View view) {
+        //set up the variables to validate not empty fields and strings to show as error messages
+        EditText etTitle = (EditText) findViewById(R.id.title_Edit);
+        String strTitle = etTitle.getText().toString();
+        String title_Error = getString(R.string.title_Post_Empty);
+
+        EditText etCapacity = (EditText) findViewById(R.id.capacity_Edit);
+        String strCapacity = etCapacity.getText().toString();
+        String capacity_Error = getString(R.string.capacity_Post_Empty);
+
+        EditText etLocation = (EditText) findViewById(R.id.location_Edit);
+        String strLocation = etLocation.getText().toString();
+        String location_Error = getString(R.string.location_Post_Empty);
+
+        EditText etPrice = (EditText) findViewById(R.id.price_Edit);
+        String strPrice = etPrice.getText().toString();
+        String price_Error = getString(R.string.price_Post_Empty);
+
+        EditText etDescription = (EditText) findViewById(R.id.description_Edit);
+        String strDescription = etDescription.getText().toString();
+        String description_Error = getString(R.string.description_Post_Empty);
+
+        EditText etDate =(EditText) findViewById(R.id.date_Edit);
+        String strDate = etDate.getText().toString();
+        String date_Error = getString(R.string.date_Post_Empty);
+
+
+
+        if(TextUtils.isEmpty(strTitle)) {
+            etTitle.setError(title_Error);
+            return;
+        }
+        else if (TextUtils.isEmpty(strCapacity)){
+            etCapacity.setError(capacity_Error);
+            return;
+        }
+        else if (TextUtils.isEmpty(strLocation)){
+            etLocation.setError(location_Error);
+            return;
+        }
+        else if (TextUtils.isEmpty(strPrice)){
+            etPrice.setError(price_Error);
+            return;
+        }
+        else if (TextUtils.isEmpty(strDescription)){
+            etDescription.setError(description_Error);
+            return;
+        }
+        else if (TextUtils.isEmpty(strDate)){
+            etDate.setError(date_Error);
+            return;
+        }
+
 
     }
 }
