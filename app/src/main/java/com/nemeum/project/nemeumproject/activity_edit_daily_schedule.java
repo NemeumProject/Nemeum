4c2f307e334388bdd9b46102afe4e67df70d8e3c
AntonyMartinez <antony11995@hotmail.com>
2019-01-04 16:03:34 -0500

    Fill-in spinner from database for edit_Daily_Schedule activity.
    Signed-off-by: AntonyMartinez <antony11995@hotmail.com>

--- app/src/main/java/com/nemeum/project/nemeumproject/activity_edit_daily_schedule.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/activity_edit_daily_schedule.java
@@ -3,6 +3,8 @@ package com.nemeum.project.nemeumproject;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,24 +19,50 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import models.Scenario;
 
 public class activity_edit_daily_schedule extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     EditText date_EditText;
     EditText startingTime_EditText;
     EditText endingTime_EditText;
+
+    List<Scenario> listScenario = new ArrayList<>();
+    ArrayList<String>scenarioList;
+
+    private String idCompany;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_daily_schedule);
 
+        SharedPreferences shared = getSharedPreferences(getResources().getString(R.string.userTypeSP), MODE_PRIVATE);
+        idCompany = (shared.getString("idCompany", ""));
+
+        getAllScenarios(idCompany);
+
         Spinner spinner = findViewById(R.id.scenarios_Spinner_Daily_Schedule);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
-                (this,R.array.scenarios_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, scenarioList);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
@@ -142,16 +170,16 @@ public class activity_edit_daily_schedule extends AppCompatActivity implements A
     public void save_Button_Daily_Schedule(View view) {
 
         EditText etDate = findViewById(R.id.et_Date_Daily_Schedule);
-        String str_date = etDate.getText().toString();
+        final String str_date = etDate.getText().toString();
         String date_Error= getString(R.string.date_Post_Empty);
 
         EditText etStartingTime = findViewById(R.id.et_StartingTime_Daily_Schedule);
-        String stStartingTime = etStartingTime.getText().toString();
+        final String stStartingTime = etStartingTime.getText().toString();
         String stStartingTime_Error= getString(R.string.stime_error);
 
 
         EditText etEndingTime = findViewById(R.id.et_EndingTime_Daily_Schedule);
-        String strEndTime = etEndingTime.getText().toString();
+        final String strEndTime = etEndingTime.getText().toString();
         String strEndingtime_Error = getString(R.string.etime_error) ;
 
         EditText etCustomerName = findViewById(R.id.et_customerName);
@@ -159,11 +187,28 @@ public class activity_edit_daily_schedule extends AppCompatActivity implements A
         String name_Error = getString(R.string.name_UserEdition_Empty);
 
         EditText etCustomerPhone = findViewById(R.id.et_customerPhone);
-        String strPhone = etCustomerPhone.getText().toString();
+        final String strPhone = etCustomerPhone.getText().toString();
         String phone_Error = getString(R.string.user_Phone_Empty);
+
+        EditText etEmail = findViewById(R.id.et_customerEmail);
+        final String strEmail = etEmail.getText().toString();
 
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
+
+        Spinner spinner = findViewById(R.id.scenarios_Spinner_Daily_Schedule);
+        String scenarios_spinner_Error = getString(R.string.scenarios_spinner_Error);
+
+        // check if spinner item is selected
+        if (spinner.getSelectedItemId() == -1)
+        {
+
+
+            Toast toast = Toast.makeText(context, scenarios_spinner_Error, duration);
+            toast.show();
+            return;
+        }
+
 
         if(TextUtils.isEmpty(str_date)) {
             etDate.setError(date_Error);
@@ -188,18 +233,121 @@ public class activity_edit_daily_schedule extends AppCompatActivity implements A
             etCustomerPhone.setError(phone_Error);
             return;
         }
+        else{
 
-        // check if spinner item is selected
-        Spinner spinner = findViewById(R.id.scenarios_Spinner_Daily_Schedule);
-        String scenarios_spinner_Error = getString(R.string.scenarios_spinner_Error);
+      //Code to sent data to the web service and save in the database
 
-        if (spinner.getSelectedItemId() == -1)
-        {
+            new Thread(new Runnable() {
+                public void run() {
+                    HttpClient httpclient = new DefaultHttpClient();
+                    HttpPost httppost = new HttpPost(getResources().getString(R.string.urlDB) + getResources().getString(R.string.joinScenarioDB));
+
+                    JSONObject postData = new JSONObject();
+                    try {
+                       // postData.put(getResources().getString(R.string.userScenario_userscenarioJson), Integer.parseInt(userScenario) );
+                       // postData.put(getResources().getString(R.string.userScenario_idUserJson), Integer.parseInt(iduser));
+                        postData.put(getResources().getString(R.string.userScenario_idScenarioJson), Integer.parseInt(/*idScenario*/null));
+                        postData.put(getResources().getString(R.string.userScenario_dateBookingJson), str_date );
+                        postData.put(getResources().getString(R.string.userScenario_startScenarioJson), stStartingTime);
+                        postData.put(getResources().getString(R.string.userScenario_endScenarioJson), strEndTime);
+                        postData.put(getResources().getString(R.string.userScenario_phoneJson), Integer.parseInt(strPhone));
+                        postData.put(getResources().getString(R.string.userScenario_emailJson), strEmail);
+
+                        StringEntity se = new StringEntity(postData.toString());
+                        httppost.setHeader(getResources().getString(R.string.dbAccessAccept), getResources().getString(R.string.dbAccessAppJson));
+                        httppost.setHeader(getResources().getString(R.string.dbAccessContentType), getResources().getString(R.string.dbAccessAppJson));
+                        httppost.setEntity(se);
+                        HttpResponse response = httpclient.execute(httppost);
+                        if(response.getStatusLine().getStatusCode() == 200){
+                            activity_edit_daily_schedule.this.runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Toast.makeText(activity_edit_daily_schedule.this,getResources().getString(R.string.bookingOK),Toast.LENGTH_LONG).show();
+                                }
+                            });
+                            Intent intent1 = new Intent(activity_edit_daily_schedule.this, UserCompanyLoginActivity.class);
+                            startActivity(intent1);
+                        }else{
+                            activity_edit_daily_schedule.this.runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Toast.makeText(activity_edit_daily_schedule.this,getResources().getString(R.string.bookingError),Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    } catch (IOException e) {
+                        activity_edit_daily_schedule.this.runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast.makeText(activity_edit_daily_schedule.this, getResources().getString(R.string.noconnection), Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
 
 
-            Toast toast = Toast.makeText(context, scenarios_spinner_Error, duration);
-            toast.show();
         }
 
+    }
+    public synchronized void getAllScenarios(final String idCompany) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                int numResults = 0;
+                BufferedReader in;
+                String data = null;
+                String line;
+                JSONArray parserList;
+                JSONObject parser;
+
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+
+                try{
+
+                    URI website = new URI(getResources().getString(R.string.urlDB) + getResources().getString(R.string.scenariosDB) + "/company/" + idCompany);
+                    request.setURI(website);
+                    HttpResponse response = httpclient.execute(request);
+                    in = new BufferedReader(new InputStreamReader(
+                            response.getEntity().getContent()));
+
+                    while((line = in.readLine()) != null)
+                        data += line;
+
+                    data = data.replaceFirst("null", "");
+                    parserList = new JSONArray(data);
+
+                    while(numResults < parserList.length()) {
+                        parser = (JSONObject) parserList.get(numResults);
+                        Scenario scenario = new Scenario();
+                        scenario.setIdScenario(parser.getInt(getResources().getString(R.string.scenarioIdJson)));
+                        scenario.setTitle(parser.getString("title"));
+
+                        numResults++;
+                        listScenario.add(scenario);
+
+                    }
+                    getList_to_Spinner();
+
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                Thread.yield();
+            }
+        }).start();
+    }
+
+    private void getList_to_Spinner(){
+        scenarioList= new ArrayList<String>();
+        scenarioList.add(getResources().getString(R.string.selectplease));
+
+        for(Scenario scenario : listScenario){
+
+            scenarioList.add(scenario.getTitle());
+
+
+        }
     }
 }
