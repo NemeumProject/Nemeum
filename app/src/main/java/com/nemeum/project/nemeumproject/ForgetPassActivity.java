c0182baef13f29ae4c34c4397d87a6088b57c33d
Maju Sumanto <maju.tampubolon@gmail.com>
2019-01-08 04:01:07 +0100

    Forget Password Screen

--- app/src/main/java/com/nemeum/project/nemeumproject/ForgetPassActivity.java
+++ app/src/main/java/com/nemeum/project/nemeumproject/ForgetPassActivity.java
@@ -2,6 +2,8 @@ package com.nemeum.project.nemeumproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgetPassActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ForgetPassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_pass);
+    }
+
+    public void getBack(View view) {
+        finish();
     }
 }
